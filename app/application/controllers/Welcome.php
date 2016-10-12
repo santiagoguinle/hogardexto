6f5d5b0d7e8ac8e29fd3b6a2d6bf1666c1d71d3e
Santiago Guinle <santiago.guinle@ventas-privadas.com>
2016-10-11 22:52:27 -0300

    fixing

--- app/application/controllers/Welcome.php
+++ app/application/controllers/Welcome.php
@@ -20,7 +20,7 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-            if(!$_SESSION["username"]){
+            if(!isset($_SESSION["username"])){
                 redirect("login");
             } else{
                 redirect("persons/create");
